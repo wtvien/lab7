b7ab6f3b4f2fc89be63a4bc452cbbbc909101f78
Whitney Vien <wtv726@gmail.com>
2017-02-22 22:35:07 -0800

    google analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,8 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(function(e) {
+		e.preventDefault();
+		ga("send", "event", "like", "click");
+	});
 }
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ var projects = require('../projects.json');
 
 exports.view = function(req, res){
   	res.render('index', projects);
+  	projects["min"] = false;
+};
+
+exports.viewMin = function(req, res){
+  	res.render('index', projects);
+  	projects["min"] = true; 
 };
